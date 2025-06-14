b09d688c98569358c5c24f4f28133c1a6060a150
oscarrryeezus <oscamn2869@gmail.com>
2025-06-13 20:12:36 -0600

    commit maton

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React OSCAR
+          Learn React OSCAR HOLA
         </a>
       </header>
     </div>
